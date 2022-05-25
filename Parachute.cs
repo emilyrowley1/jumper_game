e7f6e375646b79db932350eb0ae9b48121f36267
Emily Rowley <erowley49@gmail.com>
2022-05-25 14:19:10 -0600

    classes created

---
+++ Parachute.cs
@@ -0,0 +1,7 @@
+public class Parachute
+{
+
+}
+// display parachute method that takes in the number of strings left
+// loose life method that takes away a life from the constant
+// number of lives left
